3632b68f83086743c0c682b5aa629f42172cf3c4
Nathan Souffan <souffan.nathan@gmail.com>
2020-06-02 18:00:16 +0200

    Add a basic machine learning on hsitorical nba data

---
+++ mvp/train.py
@@ -0,0 +1,22 @@
+import sklearn
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pickle
+
+
+data = pd.read_csv('historical_data.csv')
+used_data = ['G', 'MP', 'PTS', 'TRB', 'AST', 'STL', 'BLK', 'FG%', '3P%', 'FT%']
+predict = 'Share'
+X = np.array(data[used_data])
+Y = np.array(data[predict])
+
+model = LinearRegression()
+model.fit(X, Y)
+actual = pd.read_csv('current_data.csv')
+names = actual['Player']
+actual = np.array(actual[used_data])
+predictions = model.predict(actual)
+for i in range(len(predictions)):
+    if predictions[i] > 0.2:
+        print(names[i], predictions[i])
