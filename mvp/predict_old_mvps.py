44843224851078b9bdc288439620eba639815869
Nathan Souffan <souffan.nathan@gmail.com>
2020-06-09 13:44:07 +0200

    Add the accuracy of the model in predict_old_mvps

--- mvp/predict_old_mvps.py
+++ mvp/predict_old_mvps.py
@@ -1,6 +1,7 @@
 import sklearn
 import pandas as pd
 import numpy as np
+import sys
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -28,6 +29,6 @@ for i in range(1979, 2019):
     mvp = year_data[year_data.Rank == 1]
     if mvp.iloc[0]['Player'] == predictions[0][0]:
         good_pick+=1
-    else:
-        print("in", i, mvp.iloc[0]['Player'],"would not be but", predictions[0][0], "would be")
-print(good_pick)
+    elif len(sys.argv) > 1 and sys.argv[1] == 'v':
+        print("in", str(i) + "-" + str(i+1), mvp.iloc[0]['Player'],"would not be the mvp but", predictions[0][0], "would be")
+print("accuracy: " + str(good_pick/40) + " on a 40 years span")
